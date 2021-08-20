50f1ba0652e2a39e66fde037910672c38e5d0e85
FarisJR <farisjramadhan@gmail.com>
2021-08-20 16:41:01 +0700

    merge branch

--- app/models/doctor_test_details.go
+++ app/models/doctor_test_details.go
@@ -3,6 +3,6 @@ package models
 type DoctorTestDetails struct {
 	Test_ID             int    `gorm:"not null" json:"test_id"`
 	Practice_License_ID int    `gorm:"unique;not null" json:"strid"`
-	Test                Test   `gorm:"foreignkey:ID"`
+	Tests                Tests   `gorm:"foreignkey:ID"`
 	Practice_License    Doctor `gorm:"foreignkey:ID"`
 }
