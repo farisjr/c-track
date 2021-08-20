09fe02adecf619035abd68449cccfbd19f59f113
FarisJR <farisjramadhan@gmail.com>
2021-08-20 20:21:59 +0700

    add + fix doctor feature

--- app/models/doctor.go
+++ app/models/doctor.go
@@ -4,10 +4,10 @@ import "gorm.io/gorm"
 
 type Doctor struct {
 	gorm.Model
-	Practice_License_ID      int    `gorm:"primerykey;unique;not null" json:"practice_license_id"`
-	Name                     string `gorm:"type:varchar(45);not null" json:"name"`
-	Medical_Facility_Name    string `gorm:"type:varchar(45);not null" json:"medical_facility_name"`
-	Medical_Facility_Address string `gorm:"type:varchar(45);not null" json:"medical_facility_address"`
-	User_ID                  string `gorm:"type:char(30);not null" json:"user_id"`
-	User                     User   `gorm:"foreignkey:ID"`
+	PracticeLicenseID      int    `gorm:"primaryKey" json:"practice_license_id" form:"practice_license_id"`
+	Name                   string `gorm:"type:varchar(45);not null" json:"name"`
+	MedicalFacilityName    string `gorm:"type:varchar(45);not null" json:"medical_facility_name"`
+	MedicalFacilityAddress string `gorm:"type:varchar(45);not null" json:"medical_facility_address"`
+	// UserID                 string `gorm:"type:char(30);not null" json:"user_id"`
+	// User                   User   `gorm:"foreignKey:ID"`
 }
