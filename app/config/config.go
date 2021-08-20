36716d2f9bfe0e603a7a802ec89584593c394f2e
FarisJR <farisjramadhan@gmail.com>
2021-08-20 15:43:41 +0700

    add test categories feature

--- app/config/config.go
+++ app/config/config.go
@@ -13,8 +13,8 @@ var DB *gorm.DB
 var HTTP_PORT int
 
 func InitDb() {
-	//connectionString := "usrnm:pwd@tcp(localhost:3306)/c-track?charset=utf8&parseTime=True&loc=Local"
-	connectionString := os.Getenv("CONNECTION_STRING")
+	connectionString := "root:toor@tcp(localhost:3306)/ctrack2?charset=utf8&parseTime=True&loc=Local"
+	//connectionString := os.Getenv("CONNECTION_STRING")
 	var err error
 	DB, err = gorm.Open(mysql.Open(connectionString), &gorm.Config{})
 	if err != nil {
@@ -32,13 +32,13 @@ func InitPort() {
 }
 
 func InitMigrate() {
-	DB.AutoMigrate(&models.User{})
-	DB.AutoMigrate(&models.Checker{})
-	DB.AutoMigrate(&models.Citizen{})
+	// DB.AutoMigrate(&models.User{})
+	// DB.AutoMigrate(&models.Checker{})
+	// DB.AutoMigrate(&models.Patient{})
 	DB.AutoMigrate(&models.Doctor{})
 	DB.AutoMigrate(&models.DoctorTestDetails{})
-	DB.AutoMigrate(&models.CheckerTestDetails{})
-	DB.AutoMigrate(&models.Test_Category{})
+	// DB.AutoMigrate(&models.CheckerTestDetails{})
+	DB.AutoMigrate(&models.TestCategories{})
 	DB.AutoMigrate(&models.Test{})
-	DB.AutoMigrate(&models.User{})
+	// DB.AutoMigrate(&models.User{})
 }
--- app/main.go
+++ app/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main () {
-	
+import (
+	"app/config"
+	"app/routes"
+	"fmt"
+
+	"github.com/labstack/echo"
+)
+
+func main() {
+	e := echo.New()
+	config.InitDb()
+	config.InitPort()
+	routes.New(e)
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", config.HTTP_PORT)))
 }
